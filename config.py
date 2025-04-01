3e95918ebd3180e8e2a6dac285ac7e29a55eee1d
Sarth Gaikwad <145583197+sarthsg17@users.noreply.github.com>
2025-04-01 11:20:15 +0530

    ...

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 import os
 
-DATABASE_URL = "postgresql://postgres:psql123@localhost/travel_app_db"
+DATABASE_URL = "postgresql://neondb_owner:npg_CxmTc21NOPGh@ep-icy-scene-a12w99vq-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
